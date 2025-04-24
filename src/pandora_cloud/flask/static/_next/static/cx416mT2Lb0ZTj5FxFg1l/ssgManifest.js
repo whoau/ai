08f5bd8301ecc3303d49f510528d365033d187bc
whoau <36981275+whoau@users.noreply.github.com>
2025-04-24 16:33:20 +0800

    Update ssgManifest.js

--- src/pandora_cloud/flask/static/_next/static/cx416mT2Lb0ZTj5FxFg1l/ssgManifest.js
+++ src/pandora_cloud/flask/static/_next/static/cx416mT2Lb0ZTj5FxFg1l/ssgManifest.js
@@ -1,1 +1,6 @@
-self.__SSG_MANIFEST=new Set,self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB();
+// 初始化一个空的集合，可根据需求存储一些数据
+window.customManifest = new Set();
+// 检查是否有自定义的回调函数，如果有则调用
+if (window.customManifestCallback) {
+    window.customManifestCallback();
+}
